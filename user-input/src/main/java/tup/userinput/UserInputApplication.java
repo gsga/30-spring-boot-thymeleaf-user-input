12882c800e63868a56e456218d5962b6a200d340
gsga <ggarciaa@gmail.com>
2022-08-05 09:43:16 -0300

    2022-08-02

---
+++ user-input/src/main/java/tup/userinput/UserInputApplication.java
@@ -0,0 +1,13 @@
+package tup.userinput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserInputApplication.class, args);
+	}
+
+}
---
+++ user-input/src/test/java/tup/userinput/UserInputApplicationTests.java
@@ -0,0 +1,13 @@
+package tup.userinput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
